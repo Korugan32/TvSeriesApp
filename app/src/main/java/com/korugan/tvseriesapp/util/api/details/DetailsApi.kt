600728c114c8070110c27e4a20d0ac73007d9736
Korugan32 <tahacansenel@gmail.com>
2024-10-14 14:51:19 +0300

    Create DetailsApi

---
+++ app/src/main/java/com/korugan/tvseriesapp/util/api/details/DetailsApi.kt
@@ -0,0 +1,13 @@
+package com.korugan.tvseriesapp.util.api.details
+
+import com.korugan.tvseriesapp.util.api.details.data.DetailsModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface DetailsApi {
+    @GET("/show-details")
+    suspend fun getPopular(
+        @Query("q") query : String,
+    ) : Response<DetailsModel>
+}
