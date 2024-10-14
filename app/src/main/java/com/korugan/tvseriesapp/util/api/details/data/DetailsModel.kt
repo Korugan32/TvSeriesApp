7adbf666c727e53542fc4e6c1c6adb99d3a6aec2
Korugan32 <tahacansenel@gmail.com>
2024-10-14 14:51:33 +0300

    Create Details Data

---
+++ app/src/main/java/com/korugan/tvseriesapp/util/api/details/data/DetailsModel.kt
@@ -0,0 +1,5 @@
+package com.korugan.tvseriesapp.util.api.details.data
+
+data class DetailsModel(
+    val tvShow: TvShow
+)
---
+++ app/src/main/java/com/korugan/tvseriesapp/util/api/details/data/Episode.kt
@@ -0,0 +1,8 @@
+package com.korugan.tvseriesapp.util.api.details.data
+
+data class Episode(
+    val air_date: String,
+    val episode: Int,
+    val name: String,
+    val season: Int
+)
