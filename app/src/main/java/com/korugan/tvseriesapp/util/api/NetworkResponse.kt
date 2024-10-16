ef0d3665656c96c821ada8c104ff02483a381dab
Korugan32 <tahacansenel@gmail.com>
2024-10-16 11:54:25 +0300

    Create NetworkResponse

---
+++ app/src/main/java/com/korugan/tvseriesapp/util/api/NetworkResponse.kt
@@ -0,0 +1,7 @@
+package com.korugan.tvseriesapp.util.api
+
+sealed class NetworkResponse<out T> {
+    data class Success<out T>(val data: T) : NetworkResponse<T>()
+    data class Error(val message: String) : NetworkResponse<Nothing>()
+    object Loading : NetworkResponse<Nothing>()
+}
