6f1b8dde7b7e78c7eba51f760e255685364dab24
Korugan32 <tahacansenel@gmail.com>
2024-10-17 17:48:41 +0300

    Create DetailsViewModel

---
+++ app/src/main/java/com/korugan/tvseriesapp/presentation/DetailsViewModel.kt
@@ -0,0 +1,35 @@
+package com.korugan.tvseriesapp.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.korugan.tvseriesapp.util.api.NetworkResponse
+import com.korugan.tvseriesapp.util.api.RetrofitInstance
+import com.korugan.tvseriesapp.util.api.details.data.DetailsModel
+import kotlinx.coroutines.launch
+
+class DetailsViewModel : ViewModel() {
+
+    private val detailsApi = RetrofitInstance.detailsApi
+    private val detailsResult = MutableLiveData<NetworkResponse<DetailsModel>>()
+    val details_Result: LiveData<NetworkResponse<DetailsModel>> = detailsResult
+
+    fun getData(q: String) {
+        detailsResult.value = NetworkResponse.Loading
+        viewModelScope.launch {
+            try {
+                val response = detailsApi.getDetails(q)
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        detailsResult.value = NetworkResponse.Success(it)
+                    }
+                } else {
+                    detailsResult.value = NetworkResponse.Loading
+                }
+            } catch (e: Exception) {
+                detailsResult.value = NetworkResponse.Error("Error: $e")
+            }
+        }
+    }
+}
