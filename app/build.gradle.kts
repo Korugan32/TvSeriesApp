89bed9e46348f76a911b2d3ac2e748db8d3e0de5
Korugan32 <tahacansenel@gmail.com>
2024-10-17 14:37:29 +0300

    Implementation MaterialIconExtended

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,5 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata:1.7.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
 }
