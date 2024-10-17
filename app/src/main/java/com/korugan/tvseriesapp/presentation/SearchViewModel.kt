b9c76cf9df14827b31dc329636a103adb9b4d916
Korugan32 <tahacansenel@gmail.com>
2024-10-17 17:48:31 +0300

    Create SearchViewModel

---
+++ app/src/main/java/com/korugan/tvseriesapp/presentation/SearchViewModel.kt
@@ -0,0 +1,35 @@
+package com.korugan.tvseriesapp.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.korugan.tvseriesapp.util.api.NetworkResponse
+import com.korugan.tvseriesapp.util.api.RetrofitInstance
+import com.korugan.tvseriesapp.util.api.search.data.SearchModel
+import kotlinx.coroutines.launch
+
+class SearchViewModel : ViewModel() {
+
+    private val searchApi = RetrofitInstance.searchApi
+    private val searchResult = MutableLiveData<NetworkResponse<SearchModel>>()
+    val search_result: LiveData<NetworkResponse<SearchModel>> = searchResult
+
+    fun getData(q: String, page: String) {
+        searchResult.value = NetworkResponse.Loading
+        viewModelScope.launch {
+            try {
+                val response = searchApi.getSearch(q, page)
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        searchResult.value = NetworkResponse.Success(it)
+                    }
+                } else {
+                    searchResult.value = NetworkResponse.Loading
+                }
+            } catch (e: Exception) {
+                searchResult.value = NetworkResponse.Error("Error: $e")
+            }
+        }
+    }
+}
