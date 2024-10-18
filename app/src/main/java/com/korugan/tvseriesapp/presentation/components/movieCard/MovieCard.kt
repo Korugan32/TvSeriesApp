34cef3ef802733a929154284adcb379fb767c8b5
Korugan32 <tahacansenel@gmail.com>
2024-10-18 14:36:12 +0300

    Create MovieCard Component

---
+++ app/src/main/java/com/korugan/tvseriesapp/presentation/components/movieCard/MovieCard.kt
@@ -0,0 +1,98 @@
+package com.korugan.tvseriesapp.presentation.components.movieCard
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.drawable.toBitmap
+import androidx.navigation.NavHostController
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
+import com.korugan.tvseriesapp.presentation.components.movieCard.util.getAverageColor
+import com.korugan.tvseriesapp.util.api.popular.data.PopularModel
+
+
+@Composable
+fun ImageCard(navController: NavHostController, data: PopularModel, index: Int) {
+
+    val defaultColor = MaterialTheme.colorScheme.secondaryContainer
+
+    val imageState = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(data.tv_shows[index].image_thumbnail_path)
+            .size(Size.ORIGINAL)
+            .build()
+    ).state
+
+    var dominantColor by remember {
+        mutableStateOf(defaultColor)
+    }
+    Column(
+        modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clip(RoundedCornerShape(14.dp))
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color.DarkGray,
+                        dominantColor
+                    )
+                )
+            )
+            .clickable { navController.navigate("Details"+"?id=${data.tv_shows[index].id}") },
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        if (imageState is AsyncImagePainter.State.Success) {
+            dominantColor = getAverageColor(
+                imageBitmap = imageState.result.drawable.toBitmap().asImageBitmap()
+            )
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(6.dp)
+                    .height(250.dp)
+                    .clip(RoundedCornerShape(22.dp)),
+                painter = imageState.painter,
+                contentDescription = data.tv_shows[index].name,
+                contentScale = ContentScale.Crop
+            )
+        }
+        Text(
+            text = data.tv_shows[index].name,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 15.sp
+        )
+    }
+}
