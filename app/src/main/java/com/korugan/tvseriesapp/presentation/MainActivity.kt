219f9864b53ee0fa4469f63a76c36b9f76e91be7
Korugan32 <tahacansenel@gmail.com>
2024-10-24 14:07:44 +0300

    Update MainActivity

--- app/src/main/java/com/korugan/tvseriesapp/presentation/MainActivity.kt
+++ app/src/main/java/com/korugan/tvseriesapp/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.korugan.tvseriesapp
+package com.korugan.tvseriesapp.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.korugan.tvseriesapp.ui.theme.TvSeriesAppTheme
+import com.korugan.tvseriesapp.util.navigation.Navigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TvSeriesAppTheme {
-
+                Navigation()
             }
         }
     }
